df25e25b6c684a7ca1e19e9b853273a5bdd53e41
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-01-12 13:38:11 +0200

    Fixed delete & publish issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 }
+
 // else {
 //   if (!global.prisma) {
 //     global.prisma = new PrismaClient();
